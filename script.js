9e944f66398a3a648d0eb7f7c4f341c9f86c3abc
showsfornoone <51215840+showsfornoone@users.noreply.github.com>
2019-06-06 01:51:19 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+// JavaScript Document
+
+$(document).ready(function(){
+	"use strict";
+	
+
+	$("#title").click(function(){
+		$("#nav").slideDown();
+		});
+		
+	
+	
+	
+	$("#projectsTitle").click(function(){
+		$("#projects").slideToggle();
+		});
+  
+	
+});
